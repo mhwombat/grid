00238e1d03f6f641cb51796683ed02aa27e8aed2
Amy de Buitléir <amy@nualeargais.ie>
2020-04-25 18:46:56 +0100

    added profiling

--- project.nix
+++ project.nix
@@ -9,6 +9,8 @@ mkDerivation {
   testHaskellDepends = [
     base QuickCheck test-framework test-framework-quickcheck2
   ];
+  enableLibraryProfiling = true;
+  enableExecutableProfiling = true;
   homepage = "https://github.com/mhwombat/grid";
   description = "Tools for working with regular grids (graphs, lattices)";
   license = stdenv.lib.licenses.bsd3;
