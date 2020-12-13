34cfdbf5d139861e8f101182cdd6dd6ab635abc6
Amy de Buitléir <amy@nualeargais.ie>
2020-12-13 20:13:05 +0000

    streamlined nix build

--- default.nix
+++ default.nix
@@ -1,47 +1,10 @@
-{ compiler ? "ghc8101" }:
-
-let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
-
-  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-
-  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
-    overrides = hself: hsuper: {
-      "grid" =
-        hself.callCabal2nix
-          "grid"
-          (gitignore ./.)
-          {};
-    };
-  };
-
-  shell = myHaskellPackages.shellFor {
-    packages = p: [
-      p."grid"
-    ];
-    buildInputs = with pkgs.haskellPackages; [
-      myHaskellPackages.cabal-install
-      ghcid
-      ormolu
-      hlint
-      (import sources.niv {}).niv
-      pkgs.nixpkgs-fmt
-    ];
-    withHoogle = true;
-  };
-
-  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."grid");
-
-  docker = pkgs.dockerTools.buildImage {
-    name = "grid";
-    config.Cmd = [ "${exe}/bin/grid" ];
-  };
-in
-{
-  inherit shell;
-  inherit exe;
-  inherit docker;
-  inherit myHaskellPackages;
-  "grid" = myHaskellPackages."grid";
-}
+let 
+  pkgs = import <nixpkgs> { };
+in 
+  pkgs.haskellPackages.developPackage {
+    root = ./.;
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+        [ cabal-install
+        ]);
+  }
